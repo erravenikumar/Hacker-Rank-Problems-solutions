c86b2356107f10226139261c60fe6f4d1550db4b
erravenikumar <erravenikumar@yahoo.com>
2024-10-21 17:49:17 +0530

    Initial Solution

--- src/com/erraveni/GetTotalX.java
+++ src/com/erraveni/GetTotalX.java
@@ -37,16 +37,11 @@ Explanation
 2 and 4 divide evenly into 4, 8, 12 and 16.
 4, 8 and 16 divide evenly into 16, 32, 96.
 4, 8 and 16 are the only three numbers for which each element of a is a factor and each is a factor of all elements of b.*/
+
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
+import java.util.List;
+import java.util.stream.Stream;
+
 import static java.util.stream.Collectors.toList;
 
 
